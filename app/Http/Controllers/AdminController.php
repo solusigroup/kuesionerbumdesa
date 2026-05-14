d2a6a33ca45b568cadcab68ef8e80053f6882123
solusi consulting group <kurniawan@petalmail.com>
2026-05-14 08:38:42 +0700

    feat: filter lottery candidates to only those not using an application

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -139,6 +139,7 @@ class AdminController extends Controller
         
         $candidates = Kuesioner::select('*')
             ->selectRaw('(x1_1 + x1_2 + x1_3 + x1_4 + x1_5 + x2_1 + x2_2 + x2_3 + x2_4 + x2_5 + x3_1 + x3_2 + x3_3 + x3_4 + x3_5 + y1 + y2 + y3 + y4 + y5) as total_score')
+            ->where('menggunakan_aplikasi', 'Tidak')
             ->orderByDesc('total_score')
             ->limit(10)
             ->get();
@@ -151,9 +152,10 @@ class AdminController extends Controller
         // Clear old winners
         Winner::truncate();
 
-        // Get top 10 candidates
+        // Get top 10 candidates who don't use an app
         $candidates = Kuesioner::select('*')
             ->selectRaw('(x1_1 + x1_2 + x1_3 + x1_4 + x1_5 + x2_1 + x2_2 + x2_3 + x2_4 + x2_5 + x3_1 + x3_2 + x3_3 + x3_4 + x3_5 + y1 + y2 + y3 + y4 + y5) as total_score')
+            ->where('menggunakan_aplikasi', 'Tidak')
             ->orderByDesc('total_score')
             ->limit(10)
             ->get();
